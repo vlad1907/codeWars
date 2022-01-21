7a553d421ea6dfcd22a682ad09bbe7e3a5e747d7
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-01-21 20:04:05 +0200

    Find the missing letter

---
+++ 6 kyu/Find the missing letter.js
@@ -0,0 +1,7 @@
+function findMissingLetter(array) {
+    return String.fromCharCode(
+        array
+            .find((c, i) => array[i + 1].charCodeAt() - c.charCodeAt() !== 1)
+            .charCodeAt() + 1
+    );
+}
