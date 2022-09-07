d290eda2b37ca730db8becf70be0481ca88254ff
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-09-07 18:57:04 +0300

    Grasshopper-Debug

---
+++ 8 kyu/Grasshopper - Debug.js
@@ -0,0 +1,13 @@
+function weatherInfo (temp) {
+    var celsius = (temp - 32) * (5/9)
+    if (celsius > 0)
+        return (celsius + " is above freezing temperature")
+    else
+        return (celsius + " is freezing temperature")
+}
+
+function convertToCelsius (temperature) {
+    var celsius = (temperature - 32) * (5/9)
+    return temperature
+}
+
